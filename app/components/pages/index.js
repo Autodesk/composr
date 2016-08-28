440629be89f0e47ec56df2ee3fd6a128d32c6e7c
Matan Zohar <matan.zohar@autodesk.com>
2016-08-28 18:23:17 +0300

    show alpha button only when not logged in

--- app/components/pages/index.js
+++ app/components/pages/index.js
@@ -1,7 +1,7 @@
 import cx from 'classnames';
 import { connect } from 'react-redux';
 import cookie from 'react-cookie';
-
+import StoreAPI from 'StoreAPI';
 import CompositionCard from 'common/compositionCard'
 import {GridList, GridTile} from 'material-ui/GridList';
 import {RaisedButton, Popover, TextField, FlatButton} from 'material-ui';
@@ -61,6 +61,7 @@ class Index extends React.Component {
     }
 
     renderSignUpPopup() {
+
         if ( this.state.emailSignedUp === 'unsigned') {
             return [
 
@@ -76,6 +77,29 @@ class Index extends React.Component {
         } else {
             return ( <div style={{textAlign:'center', height: '35px'}}>Something went wrong... please try again later</div> )
         }
+    }
+
+    renderSignUp() {
+        if (this.props.currentUser.get('uid'))
+            return null;
+
+        return [
+            <RaisedButton id="main-action-button" label="Join Alpha" primary={true}
+                          onTouchTap={this.handleTouchTap.bind(this)}/>,
+
+            <Popover
+                open={this.state.open}
+                anchorEl={this.state.anchorEl}
+                anchorOrigin={{horizontal: 'middle', vertical: 'bottom'}}
+                targetOrigin={{horizontal: 'middle', vertical: 'top'}}
+                onRequestClose={this.handleRequestClose.bind(this)}
+                style={{padding: '20px', overflowY: 'hidden', width: '384px'}}
+                zDepth={3}
+            >
+                {this.renderSignUpPopup()}
+            </Popover>
+        ]
+
     }
 
     render() {
@@ -96,23 +120,11 @@ class Index extends React.Component {
                         <h4 id="welcome-to-composr-text"> Composr is a flexible platform for creating real-time sound reactive visualizations.</h4>
                     </div>
 
-                    <video ref='mainVideo' id="main-video" poster="https://firebasestorage.googleapis.com/v0/b/composr-cc8ff.appspot.com/o/images%2FmainVideoPoster.jpg?alt=media&token=2b799bc7-7e95-42a2-a30d-84e225313b50" playsinline autoPlay muted>
+                    <video ref='mainVideo' id="main-video" poster="https://firebasestorage.googleapis.com/v0/b/composr-cc8ff.appspot.com/o/images%2FmainVideoPoster.jpg?alt=media&token=2b799bc7-7e95-42a2-a30d-84e225313b50" autoPlay muted>
                             <source src="https://firebasestorage.googleapis.com/v0/b/composr-cc8ff.appspot.com/o/composrHeroVid.webm?alt=media&token=971ac3cc-47af-467b-8caf-9efb9e44fd2d" type="video/webm"/>
                     </video>
+                    {this.renderSignUp()}
 
-                    <RaisedButton id="main-action-button" label="Join Alpha" primary={true} onTouchTap={this.handleTouchTap.bind(this)}/>
-
-                    <Popover
-                        open={this.state.open}
-                        anchorEl={this.state.anchorEl}
-                        anchorOrigin={{horizontal: 'middle', vertical: 'bottom'}}
-                        targetOrigin={{horizontal: 'middle', vertical: 'top'}}
-                        onRequestClose={this.handleRequestClose.bind(this)}
-                        style={{padding: '20px', overflowY: 'hidden', width: '384px'}}
-                        zDepth={3}
-                    >
-                        {this.renderSignUpPopup()}
-                        </Popover>
                 </div>
 
                 <div className="main-gallery">
@@ -129,7 +141,6 @@ class Index extends React.Component {
                             {tiles}
                         </GridList>
 
-
                     </div>
                 </div>
             </div>
@@ -137,5 +148,11 @@ class Index extends React.Component {
     }
 }
 
-export default Index;
+function mapStateToProps(state) {
+    return {
+        currentUser:  StoreAPI.getCurrentUser()
+    };
+}
+
+export default connect(mapStateToProps)(Index);
 
